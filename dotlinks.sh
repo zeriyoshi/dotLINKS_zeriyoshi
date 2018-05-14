6ba729cc497871b8088e241b4b840de39643e870
zeriyoshi <zeriyoshi@gmail.com>
2018-05-14 14:56:11 +0900

    bugfix and implement upgrade

--- dotlinks.sh
+++ dotlinks.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-__VERSION='0.0.2'
-__UPSTREAM_URI='https://github.com/zeriyoshi/dotLINKS'
+__VERSION='0.0.3'
+__UPSTREAM_URI_HTTP='https://github.com/zeriyoshi/dotLINKS'
+__UPSTREAM_URI_SSH='git@github.com:zeriyoshi/dotLINKS.git'
 
 # POSIX friendly path detection.
 # reference : https://qiita.com/komeda-shinji/items/67081ef81e79281b2c7b
@@ -20,32 +21,32 @@ __SCRIPT_DIR="${__SCRIPT_PATH%/*}"
 
 export_link () ## Link symbolic links.
 {
-    __print_purple "Create symbolic link.\n"
+    print_purple "Create symbolic link.\n"
 
-    __print_yellow "Cleanup unused files? (ex: .DS_Store) [Y/n]:"
+    print_yellow "Cleanup unused files? (ex: .DS_Store) [Y/n]:"
     read CHOICE
-    __line_back
+    line_back
     if [ "$CHOICE" != 'n' ]; then
-        __print_cyan "\n  Removing unused files..."
+        print_cyan "\n  Removing unused files..."
         find $__SCRIPT_DIR/home \( -name '.DS_Store' -or -name '._*' -or -name 'Thumbs.db' -or -name 'Desktop.ini' \) -delete
         printf " done\n"
     fi
 
     printf "\n"
 
-    __print_cyan "  Target user home directory\n\n"
+    print_cyan "  Target user home directory\n\n"
     printf "    $HOME\n\n"
-    __print_cyan "  Create symbolic link(s)\n\n"
+    print_cyan "  Create symbolic link(s)\n\n"
     FILES=$(find $__SCRIPT_DIR/home -type f | sed 's/^.*home//')
     for FILE in $FILES; do
         echo "    $FILE"
     done
 
-    __print_yellow "Are you sure? [y/N]:"
+    print_yellow "Are you sure? [y/N]:"
     read CHOICE
-    __line_back
+    line_back
     if [ "$CHOICE" != 'y' ]; then
-        __print_red "Aborted.\n"
+        print_red "Aborted.\n"
         exit 1
     fi
 
@@ -56,46 +57,46 @@ export_link () ## Link symbolic links.
 
         if [ ! -d "$FILE_DIR" ]; then
             mkdir -p "$FILE_DIR"
-            __print_purple "[CREATED]"
+            print_purple "[CREATED]"
             printf " $FILE_DIR \n"
         fi
 
         if [ -f "$HOME$FILE" ]; then
-            __print_yellow "File already exists ($HOME$FILE) overwrite? [y/N]:"
+            print_yellow "File already exists ($HOME$FILE) overwrite? [y/N]:"
             read CHOICE
-            __line_back
+            line_back
             if [ "$CHOICE" != 'y' ]; then
-                __print_yellow "[SKIPPED]"
+                print_yellow "[SKIPPED]"
                 printf " $HOME$FILE\n"
                 continue
             fi
         fi
 
         ln -sf "$__SCRIPT_DIR/home$FILE" "$HOME$FILE"
-        __print_cyan "[LINKED]"
+        print_cyan "[LINKED]"
         printf "  $HOME$FILE\n"
     done
 }
 
 export_unlink () ## Unlink symbolic links.
 {
-    __print_purple "Removing symbolic link.\n"
+    print_purple "Removing symbolic link.\n"
 
     printf "\n"
 
-    __print_cyan "  Target user home directory\n\n"
+    print_cyan "  Target user home directory\n\n"
     printf "    $HOME\n\n"
-    __print_cyan "  Removing symbolic link(s)\n\n"
+    print_cyan "  Removing symbolic link(s)\n\n"
     FILES=$(find $__SCRIPT_DIR/home -type f | sed 's/^.*home//')
     for FILE in $FILES; do
         echo "    $FILE"
     done
 
-    __print_yellow "Are you sure? [y/N]:"
+    print_yellow "Are you sure? [y/N]:"
     read CHOICE
-    __line_back
+    line_back
     if [ "$CHOICE" != 'y' ]; then
-        __print_red "Aborted.\n"
+        print_red "Aborted.\n"
         exit 1
     fi
 
@@ -104,19 +105,19 @@ export_unlink () ## Unlink symbolic links.
     for FILE in $FILES; do
         if [ -L "$HOME$FILE" ]; then
             unlink "$HOME$FILE"
-            __print_cyan "[UNLINKED]"
+            print_cyan "[UNLINKED]"
             printf " $HOME$FILE\n"
         else
             if [ -f "$HOME$FILE" ]; then
-                __print_yellow "This file is not symbolic link ($HOME$FILE) force delete? [y/N]:"
+                print_yellow "This file is not symbolic link ($HOME$FILE) force delete? [y/N]:"
                 read CHOICE
-                __line_back
-                if [ "$CHOICE" != 'y' ]; then
+                line_back
+                if [ "$CHOICE" = 'y' ]; then
                     rm "$HOME$FILE"
-                    __print_red "[DELETED]"
+                    print_red "[DELETED]"
                     printf "  $HOME$FILE\n"
                 else
-                    __print_yellow "[SKIPPED]"
+                    print_yellow "[SKIPPED]"
                     printf " $HOME$FILE\n"
                 fi
             fi
@@ -127,19 +128,81 @@ export_unlink () ## Unlink symbolic links.
             UNLINK_DIR_ITEM_COUNT=$(find "$UNLINK_DIR" -maxdepth 1 -mindepth 1 | wc -l)
             if [ $UNLINK_DIR_ITEM_COUNT -eq 0 ]; then
                 rmdir "$UNLINK_DIR"
-                __print_purple "[DELETED]"
+                print_purple "[DELETED]"
                 printf "  $UNLINK_DIR\n"
             fi
         fi
     done
 }
 
+export_upgrade () ## Upgrade dotLINKS.
+{
+    print_purple "Upgrade dotLINKS.\n\n"
+
+    print_yellow "  Checking git installed status..."
+    if ! type git > /dev/null 2>&1; then
+        print_red "FAILED\n\n"
+        print_red "Upgrade process depends for git,\nPlease install and retry.\n\nAborted.\n"
+        exit 1
+    fi
+    print_cyan "OK\n"
+
+    print_yellow "  Checking git repository..."
+    if [ ! -d "$__SCRIPT_DIR/.git" ]; then
+        print_red "FAILED\n\n"
+        print_red "This directory is not git repository.\n\nAborted.\n"
+        exit 1
+    fi
+    print_cyan "OK\n"
+
+    print_yellow "  Checking remote upstream repository..."
+    REMOTE_UPSTREAM_URI=$(cd "$__SCRIPT_DIR"; git remote get-url upstream 2>/dev/null &&:)
+    if [ ! "$REMOTE_UPSTREAM_URI" = "$__UPSTREAM_URI_SSH" ] && [ ! "$REMOTE_UPSTREAM_URI" = "$__UPSTREAM_URI_HTTP" ]; then
+        print_yellow "INVALID\n"
+        print_yellow "    Setting remote upstream repository..."
+        $(cd "$__SCRIPT_DIR"; git remote add upstream "$__UPSTREAM_URI_SSH")
+        REMOTE_UPSTREAM_URI=$(cd "$__SCRIPT_DIR"; git remote get-url upstream 2>/dev/null &&:)
+        if [ ! "$REMOTE_UPSTREAM_URI" = "$__UPSTREAM_URI_SSH" ] && [ ! "$REMOTE_UPSTREAM_URI" = "$__UPSTREAM_URI_HTTP" ]; then
+            print_red "FAILED\n\nCannot add remote upstream repository.\nPlease check remote upstream manually.\n\nAborted.\n"
+            exit 1
+        else
+            print_cyan "OK\n"
+        fi
+    else
+        print_cyan "OK\n"
+    fi
+
+    print_yellow "  Fetching remote upstream repository..."
+    if ! $(cd "$__SCRIPT_DIR"; git fetch upstream 2>/dev/null); then
+        print_red "FAILED\n\nUpstream repository fetching failed.\n\nAborted.\n"
+        exit 1
+    else
+        print_cyan "OK\n"
+    fi
+
+    print_yellow "  Merging upstream repository..."
+    MERGE_STATE=$(cd "$__SCRIPT_DIR"; git merge upstream/master 2>&1)
+
+    if [ "$(echo $MERGE_STATE | cut -c1-7)" = 'Already' ]; then
+        print_cyan "OK\n\n"
+        print_cyan "dotLINKS already up to date.\n"
+    elif [ "$(echo $MERGE_STATE | cut -c1-5)" = 'error' ]; then
+        print_red "FAILED\n\nUpstream repository merging failed.\nPlease check git status.\n\n"
+        printf "$MERGE_STATE"
+        print_red "\n\nAborted.\n"
+        exit 1
+    else
+        print_cyan "OK\n\n"
+        print_cyan "dotLINKS upgrade successfully.\n"
+    fi
+}
+
 export_version () ## Show version.
 {
-    __print_cyan "dotLINKS"
+    print_cyan "dotLINKS"
     printf " version "
-    __print_yellow "$__VERSION"
-    printf " ($__UPSTREAM_URI)\n"
+    print_yellow "$__VERSION"
+    printf " ($__UPSTREAM_URI_HTTP)\n"
 }
 
 # Self documentation.
@@ -150,51 +213,51 @@ export_usage () ## Show usage.
     
     printf "\n"
 
-    __print_yellow "Usage:\n"
+    print_yellow "Usage:\n"
     printf " $__SCRIPT_PATH [command]\n\n"
 
-    __print_yellow "Commands:\n"
+    print_yellow "Commands:\n"
 
     grep -E '^(function\s+)?\w+ \(\)\s+\{?\s*##' $__SCRIPT_PATH | sed -e 's/{//;s/ \{1,\}/ /g;s/function //;s/() ## /<>/' | cut -c8- | awk -F '<>' '{printf " \033[35m%-20s\033[0m %s\n", $1, $2}'
 }
 
-__line_back ()
+line_back ()
 {
     printf "\033[1A\033[2K"
 }
 
-__print_red ()
+print_red ()
 {
-    __print_opt '31m' "$1"
+    print_opt '31m' "$1"
 }
 
-__print_yellow ()
+print_yellow ()
 {
-    __print_opt '33m' "$1"
+    print_opt '33m' "$1"
 }
 
-__print_cyan ()
+print_cyan ()
 {
-    __print_opt '36m' "$1"
+    print_opt '36m' "$1"
 }
 
-__print_purple ()
+print_purple ()
 {
-    __print_opt '35m' "$1"
+    print_opt '35m' "$1"
 }
 
-__print_opt ()
+print_opt ()
 {
     printf "\033[$1$2\033[0m"
 }
 
-if [ '__' != "$(echo \"$1\" | cut -c1-2)" ] && [ -n "$(type -t export_$1)" ] && [ "$(type -t export_$1)" = function ]; then
+if [ -n "$(type -t export_$1)" ] && [ "$(type -t export_$1)" = function ] ; then
     "export_$1" $@
 elif [ "$1" = '' ]; then
     export_usage
     exit 1
 else
-    __print_red "[ERROR] Invalid command"
+    print_red "[ERROR] Invalid command"
     printf " $1\n"
     export_usage
     exit 1
