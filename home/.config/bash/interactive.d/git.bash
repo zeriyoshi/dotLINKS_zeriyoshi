c18036eddabda01458af44f60e6ffa344fd1ff2a
zeriyoshi <zeriyoshi@gmail.com>
2018-05-14 13:55:00 +0900

    support other os

--- home/.config/bash/interactive.d/git.bash
+++
@@ -1,3 +0,0 @@
-# Git show dirty state.
-GIT_PS1_SHOWDIRTYSTATE=true
-export PS1='[\[\e[1;37m\]macOS\[\e[m\]:\W\[\e[31m\]$(__git_ps1 " (%s)")\[\e[m\]] $ '
---
+++ home/.config/bash/interactive.d/ps1.bash
@@ -0,0 +1,18 @@
+# Git show dirty state.
+GIT_PS1_SHOWDIRTYSTATE=true
+
+OS_NAME=''
+
+if type dmidecode > /dev/null 2>&1 && [ "$(sudo dmidecode -s system-product-name)" = 'VirtualBox' ]; then
+    OS_NAME="\e[37;44;1mVB\e[m"
+fi
+
+if [ $(uname) = 'Darwin' ]; then
+    OS_NAME="${OS_NAME}\e[37;1mmacOS\e[m"
+elif type lsb_release > /dev/null 2>&1; then
+    OS_NAME="${OS_NAME}\e[36;1m$(lsb_release -is)\e[m"
+else
+    OS_NAME="\e[33;1mUnknown\e[m"
+fi
+
+export PS1='[\['"$OS_NAME"':\W\[\e[31m\]$(__git_ps1 " (%s)")\[\e[m\]] $ '
