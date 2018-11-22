ace91db39afe7dd61f566dac43d115c8eedbeeb2
zeriyoshi <zeriyoshi@gmail.com>
2018-11-22 19:31:20 +0900

    add : phpenv support

--- home/.config/bash/rc.d/path.bash
+++ home/.config/bash/rc.d/path.bash
@@ -1,6 +1,10 @@
-export GOPATH="$HOME/Developments/golang"
-export PATH="$GOPATH/bin:$PATH"
+export GOPATH="${HOME}/Developments/golang"
+export PATH="${GOPATH}/bin:${PATH}"
 
 if [ -e "/usr/local/Cellar/qt" ]; then
     export QT_HOMEBREW=true
+fi
+
+if [ -e "${HOME}/.phpenv/bin" ]; then
+    export PATH="${HOME}/.phpenv/bin:${PATH}"
 fi
