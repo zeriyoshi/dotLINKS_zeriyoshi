37487e7c293084160e288fb579c1f68e04d35fc3
zeriyoshi <zeriyoshi@gmail.com>
2019-02-01 11:10:28 +0900

    fix : workaround for IntelliJ IDEA terminal.

--- home/.bashrc
+++ home/.bashrc
@@ -4,8 +4,8 @@ if [ -f /etc/bashrc ]; then
 fi
 
 # Load shared configures.
-if [ -d "$HOME/.config/bash/rc.d" ]; then
-    for SHARED_RC_FILE in $(find "$HOME/.config/bash/rc.d" -type f -o -type l); do
-        . "$SHARED_RC_FILE"
+if [ -d "${HOME}/.config/bash/rc.d" ]; then
+    for SHARED_RC_FILE in $(find "${HOME}/.config/bash/rc.d" -type f -o -type l); do
+        . "${SHARED_RC_FILE}"
     done
 fi
--- home/.config/bash/rc.d/interactive.bash
+++ home/.config/bash/rc.d/interactive.bash
@@ -1,9 +1,15 @@
 # Real interactive environment only configures.
 # reference : https://qiita.com/dark-space/items/cf25001f89c41341a9fd
 if [[ $- =~ i ]]; then
-    if [ -d "$HOME/.config/bash/interactive.d" ]; then
-        for INTERACTIVE_FILE in $(find "$HOME/.config/bash/interactive.d" -type f -o -type l); do
-            . "$INTERACTIVE_FILE"
+    if [ -d "${HOME}/.config/bash/interactive.d" ]; then
+        for INTERACTIVE_FILE in $(find "${HOME}/.config/bash/interactive.d" -type f -o -type l); do
+            . "${INTERACTIVE_FILE}"
+        done
+    fi
+
+    if [ ! "${TERM_PROGRAM}" = "" ] && [ -d "${HOME}/.config/bash/term.d/${TERM_PROGRAM}" ]; then
+        for TERM_FILE in $(find "${HOME}/.config/bash/term.d/${TERM_PROGRAM}" -type f -o -type l); do
+            . "${TERM_FILE}"
         done
     fi
 fi
